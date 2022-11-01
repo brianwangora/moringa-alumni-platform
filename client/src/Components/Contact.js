440444b4b554c863db2ef290280c598f0bbf519c
Brandon Temba <brandontemba560@gmail.com>
2022-11-01 09:43:50 +0300

    Contact updated

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 
 const Contact = () => {
     return (
@@ -100,5 +100,55 @@ const Contact = () => {
         </div>
     );
 }
+
+export default Contact;
+*/
+
+
+
+
+import React, { useState } from "react";
+
+const FORM_ENDPOINT = ""; // TODO - fill on the later step
+
+const Contact = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+
+  if (submitted) {
+    return (
+      <>
+        <h2>Thank you!</h2>
+        <div>We'll be in touch soon.</div>
+      </>
+    );
+  }
+
+  return (
+    <form
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+    >
+      <div>
+        <input type="text" placeholder="Your name" name="name" required />
+      </div>
+      <div>
+        <input type="email" placeholder="Email" name="email" required />
+      </div>
+      <div>
+        <textarea placeholder="Your message" name="message" required />
+      </div>
+      <div>
+        <button type="submit"> Send a message </button>
+      </div>
+    </form>
+  );
+};
 
 export default Contact;
